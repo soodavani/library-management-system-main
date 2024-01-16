c0900c496ca68b7ae2b6f22b217a5aca01319a7b
Avani Sood <avanisood0806@gmail.com>
2024-01-16 22:41:56 +0530

    exceptions handled

--- main.py
+++ main.py
@@ -42,8 +42,79 @@ def ClearScreen():
 def hide_title_label():             
     title_label.place_forget()
 
-def AddNewBookGUI():
+def ShowBooksGUI():   #All Books table             
+   
     ClearScreen()
+    hide_title_label()
+    # Defining two scrollbars
+    scroll_x = ttk.Scrollbar(frame_lhs, orient=tk.HORIZONTAL)
+    scroll_y = ttk.Scrollbar(frame_lhs, orient=tk.VERTICAL)
+    tree = ttk.Treeview(frame_lhs, columns=cs.columns, height=400, selectmode="extended", yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set)
+    scroll_y.config(command=tree.yview)
+    # vertical scrollbar: left side
+    scroll_y.pack(side=tk.LEFT, fill=tk.Y)
+    scroll_x.config(command=tree.xview)
+    # Horizontal scrollbar: at bottom
+    scroll_x.pack(side=tk.BOTTOM, fill=tk.X)
+
+    # Table headings
+    tree.heading('book_id', text='Book ID', anchor=tk.W)
+    tree.heading('book_name', text='Book Name', anchor=tk.W)
+    tree.heading('author', text='Author', anchor=tk.W)
+    tree.heading('edition', text='Edition', anchor=tk.W)
+    tree.heading('price', text='Price', anchor=tk.W)
+    tree.heading('qty', text='Quantity', anchor=tk.W)
+    tree.pack()
+
+    try:
+        curs.execute("SELECT * FROM books") 
+        rows = curs.fetchall()
+        if not rows:
+            messagebox.showinfo("Database Empty", "There is no data to show")
+        else:
+            # Populate the Treeview with fetched records
+            for row_no, row_values in enumerate(rows, start=1):
+                tree.insert("", tk.END, text=row_no, values=row_values)
+    except Exception as e:
+        messagebox.showerror("Error!", f"Error due to {str(e)}")
+
+def BookHoldersGUI_Func(): #Book Holders table
+    ClearScreen()
+    hide_title_label()
+ 
+    # Creating and configuring the Treeview widget
+    tree_x = ttk.Scrollbar(frame_lhs, orient=tk.HORIZONTAL)
+    tree_y = ttk.Scrollbar(frame_lhs, orient=tk.VERTICAL)
+    
+    columns_1 = ("book_id", "book_name", "student_roll", "student_name", "issue_date", "return_date")
+    tree_1 = ttk.Treeview(frame_lhs, columns=columns_1, height=400, selectmode="extended", yscrollcommand=tree_y.set, xscrollcommand=tree_x.set)
+    
+    tree_y.config(command=tree_1.yview)
+    tree_x.config(command=tree_1.xview)
+    tree_x.pack(side=tk.BOTTOM, fill=tk.X)
+
+    # Table headings
+    for col in columns_1:
+        tree_1.heading(col, text=col, anchor=tk.W)
+
+    tree_1.pack()
+
+    try:
+        curs.execute("SELECT * FROM records") 
+        rows = curs.fetchall()
+
+        if not rows:
+            messagebox.showinfo("Database Empty", "There is no data to show")
+        else:
+            # Populate the Treeview with fetched records
+            for row_no, row_values in enumerate(rows, start=1):        #to get records from each row
+                tree_1.insert("", tk.END, text=row_no, values=row_values)
+    except Exception as e:
+        messagebox.showerror("Error!", f"Error due to {str(e)}")
+
+def AddNewBookGUI(): #Add books window
+    ClearScreen()
+    hide_title_label()
     search_background_label = tk.Label(frame_lhs)
     search_background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -82,8 +153,7 @@ def AddNewBookGUI():
     submit_bt_1 = tk.Button(frame_lhs, text='Submit', font=(cs.font_1, 12), bd=2, command=lambda: Submit(id_entry.get(), bookname_entry.get(), author_entry.get(), edition_entry.get(), price_entry.get(), qty_entry.get()), cursor="hand2", bg=cs.color_2, fg=cs.color_3).place(x=310, y=459, width=100)
 
     return id_entry, bookname_entry, author_entry, edition_entry, price_entry, qty_entry
-#change name
-def Submit(id_entry, bookname_entry, author_entry, edition_entry, price_entry, qty_entry): #addnewbookfunction 
+def Submit(id_entry, bookname_entry, author_entry, edition_entry, price_entry, qty_entry): #Add New Book function 
     if id_entry == "" or bookname_entry == "" or author_entry == "" or edition_entry == "" or price_entry == "" or qty_entry == "":
         messagebox.showerror("Error!", "Sorry, all fields are required", parent=root)
     else:
@@ -97,8 +167,8 @@ def Submit(id_entry, bookname_entry, author_entry, edition_entry, price_entry, q
                 curs.execute("insert into books (bookid,bookname,author,edition,price,quantity) values(%s,%s,%s,%s,%s,%s)",
                                 (
                                     id_entry,
-                                    bookname_entry,
-                                    author_entry,
+                                    bookname_entry.upper(),
+                                    author_entry.upper(),
                                     edition_entry,
                                     price_entry,
                                     qty_entry
@@ -109,41 +179,8 @@ def Submit(id_entry, bookname_entry, author_entry, edition_entry, price_entry, q
         except Exception as e:
             messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
 
-def BookHoldersGUI_Func():
-    ClearScreen()
-    hide_title_label()
- 
-    # Creating and configuring the Treeview widget
-    tree_x = ttk.Scrollbar(frame_lhs, orient=tk.HORIZONTAL)
-    tree_y = ttk.Scrollbar(frame_lhs, orient=tk.VERTICAL)
-    
-    columns_1 = ("book_id", "book_name", "student_roll", "student_name", "issue_date", "return_date")
-    tree_1 = ttk.Treeview(frame_lhs, columns=columns_1, height=400, selectmode="extended", yscrollcommand=tree_y.set, xscrollcommand=tree_x.set)
-    
-    tree_y.config(command=tree_1.yview)
-    tree_x.config(command=tree_1.xview)
-    tree_x.pack(side=tk.BOTTOM, fill=tk.X)
 
-    # Table headings
-    for col in columns_1:
-        tree_1.heading(col, text=col, anchor=tk.W)
-
-    tree_1.pack()
-
-    try:
-        curs.execute("SELECT * FROM records") 
-        rows = curs.fetchall()
-
-        if not rows:
-            messagebox.showinfo("Database Empty", "There is no data to show")
-        else:
-            # Populate the Treeview with fetched records
-            for row_no, row_values in enumerate(rows, start=1):        #to get records from each row
-                tree_1.insert("", tk.END, text=row_no, values=row_values)
-    except Exception as e:
-        messagebox.showerror("Error!", f"Error due to {str(e)}")
-
-def SearchGUI():
+def SearchGUI(): #Search window
     ClearScreen()
     hide_title_label()
 
@@ -204,36 +241,8 @@ def SearchFunction(book_entry, frame_lhs, window):
         except Exception as e:
             messagebox.showerror("Error!", f"Error due to {str(e)}", parent=window)
  
-def ReturnBookGUI(): #add , ShowRecordsforReturn
-    hide_title_label()
-    ClearScreen()
-    
-    borrow_background_label = tk.Label(frame_lhs)
-    borrow_background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    borrow_image_path = "/Users/AmitSood/Documents/returnbook.jpeg"  # Replace with the actual path
-    borrow_image = Image.open(borrow_image_path)
-    borrow_image = borrow_image.resize((1070, 540), Image.ANTIALIAS)
-    borrow_background_photo = ImageTk.PhotoImage(borrow_image)
-    borrow_background_label.configure(image=borrow_background_photo)
-    borrow_background_label.image = borrow_background_photo
-
-    return_book_label = tk.Label(frame_lhs, text="Return Book", font=("Arial", 30, "bold"), bg="lightgray")
-    return_book_label.place(x=250, y=40)
-
-    roll_no_label = tk.Label(frame_lhs, text="Enter Student Roll No.", font=("Arial", 15, "bold"), bg="deep sky blue")
-    roll_no_label.place(x=210, y=140)
-
-    roll_no_entry = tk.Entry(frame_lhs, bg="gray", fg="black")
-    roll_no_entry.place(x=210, y=175, width=300)
-
-    search_button = tk.Button(frame_lhs, text='Search', font=("Arial", 12), bd=2, cursor="hand2", bg="gray", fg="black", command=lambda: ShowRecordsforReturn(roll_no_entry.get(), frame_lhs)) #add , , 
-    search_button.place(x=210, y=215, width=100)
-    show_books = tk.Button(frame_lhs, text='Display book holders', font=(cs.font_1, 12), bd=2, command=BookHoldersGUI_Func,  cursor="hand2", bg=cs.color_2, fg=cs.color_3).place(x=410, y=215, width=150)
-    
-    return roll_no_entry
-
-def IssueBookGUI():
+def IssueBookGUI(): #Issue window
     ClearScreen()
     hide_title_label()
 
@@ -273,76 +282,72 @@ def IssueBookGUI():
 
     submit_bt_1 = tk.Button(frame_lhs, text='Submit', font=(cs.font_1, 12), bd=2, command=lambda: Submit_Issue(idi_entry.get(), booknamei_entry.get(), name_entry.get(), roll_entry.get(), issue_entry.get(), return_entry.get()), cursor="hand2", bg=cs.color_2, fg=cs.color_3).place(x=310, y=459, width=100)
     show_books = tk.Button(frame_lhs, text='Display all books', font=(cs.font_1, 12), bd=2, command=ShowBooksGUI, cursor="hand2", bg=cs.color_2, fg=cs.color_3).place(x=510, y=459, width=100)
-    return idi_entry, booknamei_entry, name_entry, roll_entry, issue_entry, return_entry
-
+    return idi_entry.upper(), booknamei_entry.upper(), name_entry.upper(), roll_entry, issue_entry, return_entry
 def Submit_Issue(idi_entry, booknamei_entry, name_entry, roll_entry, issue_entry, return_entry):
     if idi_entry == "" or booknamei_entry == "" or name_entry == "" or roll_entry == "" or issue_entry == "" or return_entry == "":
         messagebox.showerror("Error!", "Sorry, all fields are required", parent=root)
     else:
         try:
             curs.execute("select * from books where bookid=%s", (idi_entry,))
-            print('hi')
             row = curs.fetchone()
-            print(row)
-            book_quantity = row[5]
-            if not row:
-                messagebox.showerror("Error!", "This book does not exist", parent=root)
+            if row == None:
+                messagebox.showerror("Error!", "This book does not exist.", parent=root)
+                
+            if row[1] != booknamei_entry.upper():
+                messagebox.showerror("Error!", "Book name does not match existing book id.", parent=root)
 
-            else:
-                curs.execute("insert into records (bookid,bookname,studentroll,studentname,issuedt,returndt) values(%s,%s,%s,%s,%s,%s)",
-                                (
-                                    idi_entry,
-                                    booknamei_entry,
-                                    roll_entry,
-                                    name_entry,
-                                    issue_entry,
-                                    return_entry
-                                ))
-                book_quantity-=1
-                curs.execute("update books set quantity=%s where bookid=%s", (book_quantity, idi_entry))
-                connection.commit()
-                messagebox.showinfo('Done!', "The data has been submitted")
+            else: 
+                book_quantity = row[5]     
+                if issue_entry > return_entry: 
+                    messagebox.showerror("Error!", "Return date must be higher than issue date", parent=root)
+                else:
+                    curs.execute("insert into records (bookid,bookname,studentroll,studentname,issuedt,returndt) values(%s,%s,%s,%s,%s,%s)",
+                                    (
+                                        idi_entry,
+                                        booknamei_entry.upper(),
+                                        roll_entry,
+                                        name_entry.upper(),
+                                        issue_entry,
+                                        return_entry
+                                    ))
+                    book_quantity-=1
+                    curs.execute("update books set quantity=%s where bookid=%s", (book_quantity, idi_entry))
+                    connection.commit()
+                    messagebox.showinfo('Done!', "The data has been submitted")
 
         except Exception as e:
             messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
 
-def ShowBooksGUI():                 
-   
-    ClearScreen()
+def ReturnBookGUI(): #Return window
     hide_title_label()
-    # Defining two scrollbars
-    scroll_x = ttk.Scrollbar(frame_lhs, orient=tk.HORIZONTAL)
-    scroll_y = ttk.Scrollbar(frame_lhs, orient=tk.VERTICAL)
-    tree = ttk.Treeview(frame_lhs, columns=cs.columns, height=400, selectmode="extended", yscrollcommand=scroll_y.set, xscrollcommand=scroll_x.set)
-    scroll_y.config(command=tree.yview)
-    # vertical scrollbar: left side
-    scroll_y.pack(side=tk.LEFT, fill=tk.Y)
-    scroll_x.config(command=tree.xview)
-    # Horizontal scrollbar: at bottom
-    scroll_x.pack(side=tk.BOTTOM, fill=tk.X)
+    ClearScreen()
+    
+    borrow_background_label = tk.Label(frame_lhs)
+    borrow_background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    # Table headings
-    tree.heading('book_id', text='Book ID', anchor=tk.W)
-    tree.heading('book_name', text='Book Name', anchor=tk.W)
-    tree.heading('author', text='Author', anchor=tk.W)
-    tree.heading('edition', text='Edition', anchor=tk.W)
-    tree.heading('price', text='Price', anchor=tk.W)
-    tree.heading('qty', text='Quantity', anchor=tk.W)
-    tree.pack()
+    borrow_image_path = "/Users/AmitSood/Documents/returnbook.jpeg"  # Replace with the actual path
+    borrow_image = Image.open(borrow_image_path)
+    borrow_image = borrow_image.resize((1070, 540), Image.ANTIALIAS)
+    borrow_background_photo = ImageTk.PhotoImage(borrow_image)
+    borrow_background_label.configure(image=borrow_background_photo)
+    borrow_background_label.image = borrow_background_photo
 
-    try:
-        curs.execute("SELECT * FROM books") 
-        rows = curs.fetchall()
-        if not rows:
-            messagebox.showinfo("Database Empty", "There is no data to show")
-        else:
-            # Populate the Treeview with fetched records
-            for row_no, row_values in enumerate(rows, start=1):
-                tree.insert("", tk.END, text=row_no, values=row_values)
-    except Exception as e:
-        messagebox.showerror("Error!", f"Error due to {str(e)}")
+    return_book_label = tk.Label(frame_lhs, text="Return Book", font=("Arial", 30, "bold"), bg="lightgray")
+    return_book_label.place(x=250, y=40)
 
-def ShowRecordsforReturn(roll_no_entry, frame_lhs):
+    roll_no_label = tk.Label(frame_lhs, text="Enter Student Roll No.", font=("Arial", 15, "bold"), bg="deep sky blue")
+    roll_no_label.place(x=210, y=140)
+
+    roll_no_entry = tk.Entry(frame_lhs, bg="gray", fg="black")
+    roll_no_entry.place(x=210, y=175, width=300)
+
+    search_button = tk.Button(frame_lhs, text='Search', font=("Arial", 12), bd=2, cursor="hand2", bg="gray", fg="black", command=lambda: ShowRecordsforReturn(roll_no_entry.get(), frame_lhs)) #add , , 
+    search_button.place(x=210, y=215, width=100)
+    show_books = tk.Button(frame_lhs, text='Display book holders', font=(cs.font_1, 12), bd=2, command=BookHoldersGUI_Func,  cursor="hand2", bg=cs.color_2, fg=cs.color_3).place(x=410, y=215, width=150)
+    
+    return roll_no_entry
+
+def ShowRecordsforReturn(roll_no_entry, frame_lhs): #Returnable records
     if roll_no_entry == "":
         messagebox.showerror("Error!", "Please enter a roll no.")
     else:
@@ -381,7 +386,21 @@ def ShowRecordsforReturn(roll_no_entry, frame_lhs):
         except Exception as e:
             messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
 
-def ReturningBook(tree_1, row, delete_button):
+def OnSelectedforReturn(event): #Selecting required record
+    try:
+        tree_1 = event.widget  # Get the Treeview widget that triggered the event
+        selection = tree_1.selection()   
+        
+        if selection:
+            item = tree_1.item(selection)
+            row = item['values']      #creating delete button #return book called after deletion
+            delete_button = tk.Button(frame_3, text='Remove record', font=(cs.font_1, 12), bd=2, command=lambda r=row: ReturningBook(tree_1, r, delete_button), cursor="hand2", bg=cs.color_2, fg=cs.color_3)
+            delete_button.place(x=50, y=0, width=100)
+        else:
+            messagebox.showerror("Error!", "No item selected", parent=root)
+    except Exception as e:
+        messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
+def ReturningBook(tree_1, row, delete_button): #Deletion of book
     try:
         status = messagebox.askokcancel('Returning Book', 'Are you sure you want to proceed?')
         if status:
@@ -402,20 +421,7 @@ def ReturningBook(tree_1, row, delete_button):
     except Exception as e:
         messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
 
-def OnSelectedforReturn(event): 
-    try:
-        tree_1 = event.widget  # Get the Treeview widget that triggered the event
-        selection = tree_1.selection()   
-        
-        if selection:
-            item = tree_1.item(selection)
-            row = item['values']      #creating delete button #return book called after deletion
-            delete_button = tk.Button(frame_3, text='Delete', font=(cs.font_1, 12), bd=2, command=lambda r=row: ReturningBook(tree_1, r, delete_button), cursor="hand2", bg=cs.color_2, fg=cs.color_3)
-            delete_button.place(x=50, y=0, width=100)
-        else:
-            messagebox.showerror("Error!", "No item selected", parent=root)
-    except Exception as e:
-        messagebox.showerror("Error!", f"Error due to {str(e)}", parent=root)
+
 
 
 #main buttons #move up to widgets
